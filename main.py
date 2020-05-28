76692d765d3aac73b08db378245c631c1ebd2d48
NidhishKumarReddy <nidhishkumarreddy@gmail.com>
2020-05-28 15:29:13 +0530

    updated main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+# Cleaning Text Steps
+# 1) Create a text file and take text from it
+# 2) Convert all the letters into lowercase
+# 3) Remove punctuations like .,!? etc.
+import string
+text = open('read.txt', encoding = 'utf-8').read()
+
+# Converting text to lower case
+lower_case = text.lower()
+
+# Removing punctuations from the string
+cleaned_text = lower_case.translate(str.maketrans('','',string.punctuation))
+
+# Creating a list to store all words 
+tokenized_words = cleaned_text.split()
+
+# Stop words are the words that doesnt add meaning to the text
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+# Removing stop words from the text
+final_words = []
+for word in tokenized_words:
+    if word not in stop_words:
+        final_words.append(word)
+
+print(final_words)
